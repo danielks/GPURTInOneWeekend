2bfc2c0102d348328685d167f7129437699c116a
Daniel K. Silva <danielks@gmail.com>
2024-06-12 13:49:48 -0300

    Adicionar arquivos de projeto.

---
+++ GPURTInOneWeekend/Program.cs
@@ -0,0 +1,93 @@
+﻿using GPURTInOneWeekend;
+using ComputeSharp;
+using Raylib_cs;
+using System.Diagnostics;
+using TerraFX.Interop.Windows;
+using System.Numerics;
+using TerraFX.Interop.DirectX;
+
+
+
+float iTime = 0f;
+Stopwatch timer = Stopwatch.StartNew();
+long frameCount = 0;
+
+Raylib.InitWindow(500, 500, "CPU Shader Toy");
+
+int renderWidth = Raylib.GetRenderWidth();
+int renderHeight = Raylib.GetRenderHeight();
+
+
+var gpu = ComputeSharp.GraphicsDevice.GetDefault();
+var gpuBuffer = gpu.AllocateReadWriteBuffer<uint>(renderWidth * renderHeight);
+
+
+Stopwatch sw = new Stopwatch();
+double frameTime = sw.Elapsed.TotalMilliseconds;
+
+while (!Raylib.WindowShouldClose())
+{
+    sw.Restart();
+
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.White);
+
+    iTime = (float)timer.Elapsed.TotalSeconds;
+    var shader = new RayTracerComputeShader(gpuBuffer, iTime, renderWidth);
+
+    gpu.For(renderWidth, renderHeight, shader);
+    var buffer = new uint[renderWidth * renderHeight];
+    gpuBuffer.CopyTo(buffer);
+
+    for (int i = 0; i < renderWidth; i++)
+    {
+        for (int j = 0; j < renderHeight; j++)
+        {
+            var c = buffer[j * renderWidth + i];
+
+            var color = new Raylib_cs.Color(
+                (byte)(c >> 24),
+                (byte)(c >> 16),
+                (byte)(c >> 8),
+                (byte)255
+
+                );
+
+            Raylib.DrawPixel(i, j, color);
+        }
+    }
+
+    /*
+    Image i2 = new Image
+    {
+        Format = PixelFormat.UncompressedR8G8B8A8,
+        Width = renderWidth,
+        Height = renderHeight,
+        Mipmaps = 1
+    };
+    Raylib_cs.Texture2D t2 = Raylib.LoadTextureFromImage(i2);
+
+    
+    unsafe
+    {
+        fixed (byte* bPtr = &buffer[0])
+        {
+            Raylib.UpdateTexture(t2, bPtr);
+            Raylib.DrawTexture(t2, 0, 0, Color.White);
+        }
+    }*/
+
+    Raylib.EndDrawing();
+
+    sw.Stop();
+    frameTime = sw.Elapsed.TotalMilliseconds;
+
+    if (frameCount % 50 == 0) //a cada 50 frames mostra o ultimo frametime
+    {
+        Raylib.SetWindowTitle(string.Format("{0}ms. FPS: {1}", frameTime, 1000.0f / frameTime));
+    }
+
+    frameCount++;
+}
+
+Raylib.CloseWindow();
---
+++ GPURTInOneWeekend/RayTracerComputeShader.cs
@@ -0,0 +1,51 @@
+﻿using ComputeSharp;
+using System.Numerics;
+using TerraFX.Interop.Windows;
+
+namespace GPURTInOneWeekend
+{
+    [AutoConstructor]
+    [EmbeddedBytecode(DispatchAxis.XY)]
+    internal readonly partial struct RayTracerComputeShader : IComputeShader
+    {
+        private readonly ReadWriteBuffer<uint> buffer;
+        public readonly float time;
+        public readonly int renderWidth;
+        public void Execute()
+        {
+            //boilerplate code
+            var fragCoord = new Vector2(ThreadIds.X, ThreadIds.Y);
+            
+            
+            
+
+            
+
+
+
+
+
+
+            float colX = Hlsl.Abs(Hlsl.Cos(time));
+            float colY = Hlsl.Abs(Hlsl.Sin(time));
+            float colZ = Hlsl.Abs(Hlsl.Cos(time));
+
+            Vector3 col = new Vector3(colX, colY, colZ);
+
+            col.X = 1;
+            col.Y = 0;
+            col.Z = 0;
+
+
+            int idx = (ThreadIds.Y * renderWidth + ThreadIds.X);
+            buffer[idx] = (uint)(col.X * 255) << 24;
+            buffer[idx] = buffer[idx] | ((uint)(col.Y * 255) << 16);
+            buffer[idx] = buffer[idx] | ((uint)(col.Z * 255) << 8);
+            buffer[idx] = buffer[idx] | ((uint)255);
+
+
+
+
+        }
+    }
+}
